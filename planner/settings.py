207c960e5b5126ca8d79e0597bd0e9a83fa84899
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2024-09-22 14:20:56 +0200

    Update settings.py to use PostgreSQL in Docker

--- planner/settings.py
+++ planner/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "planner.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'django_user',
+        'PASSWORD': 'django_pass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
