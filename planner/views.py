569653b40ff9d94a3271500871cb383e624b24b2
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2024-09-23 20:48:57 +0200

    Add a second blank line before function definition to meet PEP8 style standards

--- planner/views.py
+++ planner/views.py
@@ -1,6 +1,7 @@
 """Views for the planner application."""
 from django.shortcuts import render
 
+
 def home(request):
     """Render the home page."""
     return render(request, 'planner/home.html')
