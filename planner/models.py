ebeda36a7159f4ed4376fcb204a45f16bcc74ac5
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2024-09-24 02:39:16 +0200

    Add Task model object

--- planner/models.py
+++ planner/models.py
@@ -1,6 +1,11 @@
 """Models for the planner application."""
 from django.db import models
-from .choices import MOOD_CHOICES, PRODUCTIVITY_CHOICES
+from .choices import (
+    MOOD_CHOICES,
+    PRODUCTIVITY_CHOICES,
+    IMPORTANCE_CHOICES,
+    ESTIMATE_CHOICES,
+)
 
 
 class WorkingDay(models.Model):
@@ -13,3 +18,19 @@ class WorkingDay(models.Model):
 
     def __str__(self) -> str:
         return f"Working Day: {self.date}"
+
+
+class Task(models.Model):
+    """Placeholder docstring for Task model."""
+
+    task_name = models.CharField(max_length=255)
+    importance = models.IntegerField(choices=IMPORTANCE_CHOICES)
+    estimate = models.IntegerField(choices=ESTIMATE_CHOICES)
+    working_day = models.ForeignKey(
+        WorkingDay,
+        related_name='tasks',
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return f"Task: {self.task_name}"
