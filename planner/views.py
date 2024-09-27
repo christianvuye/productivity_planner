64fa0e50a7a6b8307390caed16c1aa87663dd024
Christian Vuye <christianvuye@Christians-MacBook-Pro.local>
2024-09-27 19:27:20 +0200

    Update handle post and get request functions with helper function to initialize forms and reduce repeating code - DRY principle

--- planner/views.py
+++ planner/views.py
@@ -5,58 +5,48 @@ from django.http import HttpRequest, HttpResponse
 from .forms import WorkingDayForm, TaskForm
 from .models import WorkingDay
 from .choices import MOOD_AVERAGE, PRODUCTIVITY_AVERAGE
-from .helpers import save_task
+from .helpers import save_task, initialize_forms
 
 
 def handle_post(request: HttpRequest, working_day: WorkingDay) -> HttpResponse:
     """Handle the POST request for the home page."""
-    working_day_form = WorkingDayForm(request.POST, instance=working_day)
-    main_task_form = TaskForm(request.POST, prefix='main_task')
-    secondary_task_form_1 = TaskForm(request.POST, prefix='secondary_task_1')
-    secondary_task_form_2 = TaskForm(request.POST, prefix='secondary_task_2')
-    additional_task_form_1 = TaskForm(request.POST, prefix='additional_task_1')
-    additional_task_form_2 = TaskForm(request.POST, prefix='additional_task_2')
+    forms = initialize_forms(request, working_day)
 
-    if working_day_form.is_valid():
-        working_day_form.save()
+    if forms['working_day_form'].is_valid():
+        forms['working_day_form'].save()
 
-        if main_task_form.is_valid():
-            save_task(main_task_form, working_day)
+        if forms['main_task_form'].is_valid():
+            save_task(forms['main_task_form'], working_day)
 
-        if secondary_task_form_1.is_valid():
-            save_task(secondary_task_form_1, working_day)
+        if forms['secondary_task_form_1'].is_valid():
+            save_task(forms['secondary_task_form_1'], working_day)
 
-        if secondary_task_form_2.is_valid():
-            save_task(secondary_task_form_2, working_day)
+        if forms['secondary_task_form_2'].is_valid():
+            save_task(forms['secondary_task_form_2'], working_day)
 
-        if additional_task_form_1.is_valid():
-            save_task(additional_task_form_1, working_day)
+        if forms['additional_task_form_1'].is_valid():
+            save_task(forms['additional_task_form_1'], working_day)
 
-        if additional_task_form_2.is_valid():
-            save_task(additional_task_form_2, working_day)
+        if forms['additional_task_form_2'].is_valid():
+            save_task(forms['additional_task_form_2'], working_day)
 
         return redirect('home')
 
     return render_home_page(
-        request, working_day_form, main_task_form,
-        secondary_task_form_1, secondary_task_form_2,
-        additional_task_form_1, additional_task_form_2
+        request, forms['working_day_form'], forms['main_task_form'],
+        forms['secondary_task_form_1'], forms['secondary_task_form_2'],
+        forms['additional_task_form_1'], forms['additional_task_form_2']
     )
 
 
 def handle_get(request: HttpRequest, working_day: WorkingDay) -> HttpResponse:
     """Handle the GET request for the home page."""
-    working_day_form = WorkingDayForm(request.POST, instance=working_day)
-    main_task_form = TaskForm(request.POST, prefix='main_task')
-    secondary_task_form_1 = TaskForm(request.POST, prefix='secondary_task_1')
-    secondary_task_form_2 = TaskForm(request.POST, prefix='secondary_task_2')
-    additional_task_form_1 = TaskForm(request.POST, prefix='additional_task_1')
-    additional_task_form_2 = TaskForm(request.POST, prefix='additional_task_2')
+    forms = initialize_forms(request, working_day)
 
     return render_home_page(
-        request, working_day_form, main_task_form,
-        secondary_task_form_1, secondary_task_form_2,
-        additional_task_form_1, additional_task_form_2
+        request, forms['working_day_form'], forms['main_task_form'],
+        forms['secondary_task_form_1'], forms['secondary_task_form_2'],
+        forms['additional_task_form_1'], forms['additional_task_form_2']
     )
 
 
